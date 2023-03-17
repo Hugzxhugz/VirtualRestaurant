a76b01405aff3562e952d41aed2b618d4b3bfc8b
Hugzxhugz <jameskenthagman@gmail.com>
2023-03-17 10:59:57 +0100

    Finished the restaurant class. No implementation to the functions yet.

---
+++ VirtualRestaurant/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿namespace VirtualRestaurant;
+
+public class Restaurant
+{
+    public Dictionary<string, decimal> Menu;
+    public List<String> ordersList;
+    
+    
+    public Restaurant(Dictionary<string, decimal> menu, List<string> ordersList)
+    {
+        Menu = menu;
+        this.ordersList = ordersList;
+    }
+
+
+    public void TakeOrder()
+    {
+        
+    }
+    
+    public void ReleaseOrder()
+    {
+        
+    }
+}
